99c2023cf5d94a817450386b9f12e7acf18c453b
Tristan Slater <hello@trslater.ca>
2023-08-24 14:16:01 -0700

    Make CLI actually do something

--- src/fauxproj/cli.py
+++ src/fauxproj/cli.py
@@ -1,7 +1,32 @@
 import sys
+import textwrap
+
+import numpy as np
+
+USAGE = textwrap.dedent("""\
+    Usage: fauxproj N
+
+        Returns the square root of N""")
 
 
 def run():
-    message = sys.argv[1]
+    if len(sys.argv) < 2:
+        print(USAGE)
+        exit(1)
 
-    print(message)
+    try:
+        n = float(sys.argv[1])
+    
+    except ValueError:
+        print("ERROR: N must be a number", file=sys.stderr)
+        print()
+        print(USAGE)
+        exit(1)
+
+    if n < 0:
+        print("ERROR: N non-negative", file=sys.stderr)
+        print()
+        print(USAGE)
+        exit(1)
+
+    print(np.sqrt(n))
