f06c9460023997127e6e8b073683f835b71fab16
Tristan Slater <hello@trslater.ca>
2023-08-24 17:44:55 -0700

    Pass CLI test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,9 +2,9 @@ from fauxproj import cli
 
 
 def test_run(monkeypatch, captured_stdout):
-    message = "foo"
-    monkeypatch.setattr("sys.argv", [None, message])
+    N = 2
+    monkeypatch.setattr("sys.argv", [None, N])
     
     cli.run()
 
-    assert captured_stdout.getvalue() == f"{message}\n"
+    assert captured_stdout.getvalue() == "1.4142135623730951\n"
