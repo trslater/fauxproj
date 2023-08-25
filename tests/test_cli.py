8b3a3cce997c9129d85fb764ff9d596619da6448
Tristan Slater <hello@trslater.ca>
2023-08-24 18:15:50 -0700

    Remove blank line whitespace

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,7 +4,7 @@ from fauxproj import cli
 def test_run(monkeypatch, captured_stdout):
     N = 2
     monkeypatch.setattr("sys.argv", [None, N])
-    
+
     cli.run()
 
     assert captured_stdout.getvalue() == "1.4142135623730951\n"
